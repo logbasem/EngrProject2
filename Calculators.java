f5505d424462250bf0a52a7579b67c6bc76e10ab
logbasem <logbasem@gmail.com>
2022-10-27 18:42:48 -0500

    First part of class

--- Calculators.java
+++ Calculators.java
@@ -1,4 +1,36 @@
-
+/**"Prototype" class for storing different mathematical functions for calculator website.
+ * 
+ * @author logan
+ *
+ */
 public class Calculators {
-
+	private int numPeople;
+	private int wingsPerPerson;
+	private int hungerLevel;
+	//private int budget;
+	private boolean fries;
+	
+	public Calculators() {
+		numPeople = 0;
+		wingsPerPerson = 0;
+		hungerLevel = 0;
+		fries = false;
+	}
+	
+	public int returnNumWings() {
+		int wingsByPeople = numPeople * wingsPerPerson;
+		
+		return wingsByPeople;
+	}
+	
+	
+	//calculate total wings * numPeople
+	
+	
+	
+	//calculate hungerLevel multiplier
+	
+	
+	//add fries if fries true
+	
 }
